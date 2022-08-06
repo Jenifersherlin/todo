d46b55d2080ff76bb773798da0ea0ce33f3b8aec
Jenifersherlin <djenifer3108@gmail.com>
2022-08-06 13:17:38 +0530

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'TODO LIST';
+  todo=[
+    {
+    label:'learn c++',
+  done:false,
+  priority:3
+},
+{
+  label:'prep',
+done:false,
+priority:2
+},
+{
+  label:'learn python',
+done:true,
+priority:1
+},
+{
+  label:'aptitude',
+done:false,
+priority:4
+},
+{
+  label:'mcq',
+done:false,
+priority:5
+},
+];
+addtodo( newtodolabel )
+{
+  var newtodo = {
+    label:newtodolabel,
+    priority:1,
+    done:false
+  };
+  this.todo.push(newtodo);
+}
+deletetodo(todo){
+  this.todo=this.todo.filter(t=>t.label !== todo.label );
+}
 }
